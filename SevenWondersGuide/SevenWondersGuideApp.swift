9a650917c76984e87062589daef9cd9de5a739ba
Johnnan Monzon <jjsalomonzon@gmail.com>
2020-10-20 13:55:53 -0400

    Initial Commit

---
+++ SevenWondersGuide/SevenWondersGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenWondersGuideApp.swift
+//  SevenWondersGuide
+//
+//  Created by Johnnan Monzon on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct SevenWondersGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
